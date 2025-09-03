4e191b1c116c12af0c9c1af30c5633dc2777edf6
ErennTunali <156017553+ErennTunali@users.noreply.github.com>
2025-09-03 08:29:10 +0300

    added database.py and tasks.db, coded database init function

---
+++ database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+def connect(path):
+    connection = sqlite3.connect(path)
+    return connection
+
+
+
+def initDB(path):
+    conn = connect(path)
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT NOT NULL, done BOOL DEFAULT 0)')
+    conn.commit()
+    conn.close()
+
+    
+
+
+    
---
+++ main.py
@@ -0,0 +1,26 @@
+from database import *
+
+databasePath = '/Users/erentunali/repos/CLI-Based-To-Do-App/tasks.db'
+
+while (True):
+    print('Welcome to the task manager\n\n')
+    print('''
+Commands:
+          init: Create Database
+          q: Exit
+        
+
+
+
+
+
+
+
+\n\n''')
+    command = input('Enter Command: ')
+    if (command == 'q'):
+        break
+    elif command == 'init':
+        initDB(databasePath)
+        print('DB initiliazed')
+    
